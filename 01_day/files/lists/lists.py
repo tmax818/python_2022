b381cbc2671f14e949b5a57f482c3a745a1cd52d
tmax818 <tylermaxwell661@gmail.com>
2022-03-06 09:59:14 -0800

    update lists

---
+++ 01_day/files/lists/lists.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+## lists.py
+## demo
+
+# x = [1,2,3,4,5,6]
+# print("x before append", x)
+# x.append(7)
+# print("x after append", x)
+
+## list slicing
+
+# start = 0 # inclusive
+# stop = len(x)# exclusive
+# step = -1
+
+# print(x[:])
+# print(x[::step])
+# print(x[::-1])
+# print(x[1:5])
+
+x = [1,2,3,4,5]
+pprint(dir(x))
