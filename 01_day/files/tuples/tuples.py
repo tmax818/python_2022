a064acab6dd67c0ec5a0b0d72f61f04f65311aa1
tmax818 <tylermaxwell661@gmail.com>
2022-03-06 11:11:06 -0800

    update

---
+++ 01_day/files/tuples/tuples.py
@@ -0,0 +1,18 @@
+
+## tuples.py
+## demo
+
+
+# my_tuple = 1,
+# print(type(my_tuple))
+
+dog = ("Canis Familiaris", "dog", "carnivore", 12)
+print(dog)
+print(id(dog))
+dog = dog + ("domestic",)
+print(dog)
+print(id(dog))
+
+
+print(dir(dog))
+
